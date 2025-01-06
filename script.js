f9a495e96a85cddb0dcc311c3097bfd3068ed43d
chenfen <237488289@qq.com>
2025-01-06 14:30:53 +0800

    Add files via upload
    更新头像图片链接

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // 模拟的好友数据
 const friends = [
     { name: '妈妈', avatar: 'https://img.ixintu.com/download/jpg/201912/a7639cdf0a9c9a69ea8594039b864678.jpg!con' },
-    { name: '大哥', avatar: 'https://img.zcool.cn/community/018fae5a9f4cfba801219586b6e26b.jpg@1280w_1l_2o_100sh.jpg' },
-    { name: '二哥', avatar: 'https://img.zcool.cn/community/01089c59426acba8012193a3786b05.jpg@1280w_1l_2o_100sh.jpg' },
-    { name: '大嫂', avatar: 'https://img.zcool.cn/community/01452a5e0af4ada80120a89566284b.jpg@1280w_1l_2o_100sh.jpg' },
-    { name: '二嫂', avatar: 'https://c-ssl.duitang.com/uploads/item/201503/24/20150324003541_RSJX8.jpeg' },
+    { name: '大哥', avatar: 'https://img.keaitupian.cn/newupload/12/1735527805928604.png' },
+    { name: '二哥', avatar: 'https://img.keaitupian.cn/newupload/12/1735526953909608.png' },
+    { name: '大嫂', avatar: 'https://img.keaitupian.cn/newupload/12/1735527805747290.png' },
+    { name: '二嫂', avatar: 'https://img.keaitupian.cn/newupload/12/1734923859639078.png' },
     { name: 'chenjiaming', avatar: 'https://www.keaitupian.cn/cjpic/frombd/2/253/1405846251/2452424234.jpg' }
 
 ];
